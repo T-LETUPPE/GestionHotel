45ec2b5d41da15dc8c448783ca43e2fb3b35c48b
T-LETUPPE <thomas.letuppe@outlook.fr>
2018-04-26 15:08:02 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args) {
+    Date bla = new Date();
+    Date blo = new Date();
+    bla.creerDate(12, 06, 2018);
+    bla.afficheDate();
+    blo.creerDate();
+    blo.afficheDate();
+  }
+}
